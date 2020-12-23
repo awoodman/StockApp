c08ffed41a2344639885bc9ad6a402e11fb1c982
Adam Woodman <adamwoodman@gmail.com>
2020-12-22 21:08:36 -0500

    install requests, start parsing json response

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
+    import requests
+    import json
+
     # pk_e57289f6fa8647d8a4a84b920f699b92
-    return render(request, 'home.html', {})
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_e57289f6fa8647d8a4a84b920f699b92")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
