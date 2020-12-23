40e99fa97fc084961531742e29649fc2038bf2ce
Adam Woodman <adamwoodman@gmail.com>
2020-12-22 20:46:20 -0500

    Add bootstrap header and home/about pages

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
+    # pk_e57289f6fa8647d8a4a84b920f699b92
     return render(request, 'home.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
