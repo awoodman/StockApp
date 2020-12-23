f153df6a83b9d86b509a34311aadaed52a305065
Adam Woodman <adamwoodman@gmail.com>
2020-12-22 21:31:15 -0500

    Form search for tickers

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -4,13 +4,18 @@ def home(request):
     import requests
     import json
 
-    # pk_e57289f6fa8647d8a4a84b920f699b92
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_e57289f6fa8647d8a4a84b920f699b92")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_e57289f6fa8647d8a4a84b920f699b92")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+
+        return render(request, 'home.html', {'api':api})
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol"})
 
     return render(request, 'home.html', {'api':api})
 
